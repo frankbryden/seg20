584a40261b7de56787e05e681c86c4cc02eaf750
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2019-03-05 16:37:59 +0000

    Refactor object and method names

--- src/CalculationsTests.java
+++ src/CalculationsTests.java
@@ -1,31 +1,25 @@
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert.*;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
-import org.junit.Before;
-import org.junit.Test;
 
 public class CalculationsTests {
 
     //Scenario 2 tests
-    private RunwayDesignator runwayDesignator = new RunwayDesignator("09R");
-    private RunwayConfig initialRunway = new RunwayConfig(runwayDesignator, 3660, 3660, 3660, 3353, 307);
-    private Calculations calculations = new Calculations(initialRunway);
-    private Obstacle obstacle = new Obstacle("testObstacle", 25);
-    private RunwayConfig recalculatedRunway = calculations.recalculateParams(obstacle, 2853, Calculations.Direction.TOWARDS);
+    private RunwayDesignator runwayDesignatorScenario2 = new RunwayDesignator("09R");
+    private RunwayConfig initialRunwayScenario2 = new RunwayConfig(runwayDesignatorScenario2, 3660, 3660, 3660, 3353, 307);
+    private Calculations calculationsScenario2 = new Calculations(initialRunwayScenario2);
+    private Obstacle obstacleScenario2 = new Obstacle("testObstacle", 25);
+    private RunwayConfig recalculatedRunwayScenario2 = calculationsScenario2.recalculateParams(obstacleScenario2, 2853, Calculations.Direction.TOWARDS);
 
 
     @Test
-    public void testNewTORA(){ assertThat(  2903, is(equalTo( recalculatedRunway.getTORA()))  ); }
-
+    public void scenario2TestNewTORA(){ assertThat(  2903, is(equalTo( recalculatedRunwayScenario2.getTORA()))  ); }
     @Test
-    public void testNewTODA(){ assertThat( 2903, is(equalTo(recalculatedRunway.getTODA()))); }
-
+    public void scenario2TestNewTODA(){ assertThat( 2903, is(equalTo(recalculatedRunwayScenario2.getTODA()))); }
     @Test
-    public void testNewASDA(){ assertThat( 2903, is(equalTo(recalculatedRunway.getASDA()))); }
-
+    public void scenario2TestNewASDA(){ assertThat( 2903, is(equalTo(recalculatedRunwayScenario2.getASDA()))); }
     @Test
-    public void testNewLDA(){ assertThat( 2393, is(equalTo(recalculatedRunway.getLDA()))); }
+    public void scenario2TestNewLDA(){ assertThat( 2393, is(equalTo(recalculatedRunwayScenario2.getLDA()))); }
 
 }
